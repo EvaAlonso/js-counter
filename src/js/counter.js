a4dc0a0c58db1b5e094a518303cfbed2e5301138
Eva Alonso Contreras <Eva.Alonso@proton.me>
2024-10-10 02:07:54 +0200

    added functionality

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,27 @@
+let counter = 0;
+const initialNumber = document.querySelector("#number");
 function add(){
     //Escribir tu código que aumenta
+    ++counter;
+    initialNumber.innerText = counter;
 }
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+   counter = 0;
+   initialNumber.innerText = counter;
 }
 
 function decrease(){
     //Escribir tu código que resta
+    if(counter >0){
+        --counter;
+        initialNumber.innerText = counter;
+    } else {
+        counter = 0;
+        initialNumber.innerText = counter;
+    }
+    
 };
 
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,12 @@
-import {add, reset, decrease} from './counter.js'
+import {add, reset, decrease} from './counter.js';
 
+
+const decreaseBtn = document.querySelector("#decrease");
+const resetBtn = document.querySelector("#reset");
+const addBtn = document.querySelector("#add");
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+
+addBtn.addEventListener("click", add);
+decreaseBtn.addEventListener("click", decrease);
+resetBtn.addEventListener("click", reset);
